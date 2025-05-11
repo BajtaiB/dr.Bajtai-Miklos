5ebb196c6b6a6cabef2a8bb17ce06b4bcdf6db6c
Ügyvéd <bhercegno1@gmail.com>
2025-05-11 18:35:31 +0200

    script.js

--- script.js
+++ script.js
@@ -4,20 +4,3 @@ ScrollReveal().reveal('#hero', { delay: 300, origin: 'bottom', distance: '50px',
 ScrollReveal().reveal('#rolam', { delay: 400, origin: 'left', distance: '50px', duration: 800 });
 ScrollReveal().reveal('#szolgaltatasok', { delay: 500, origin: 'right', distance: '50px', duration: 800 });
 ScrollReveal().reveal('#kapcsolat', { delay: 600, origin: 'bottom', distance: '50px', duration: 800 });
-
-// Email küldés (emailjs használata)
-(function() {
-    emailjs.init(nHJ_29EAVqqSMMbo9); 
-})();
-
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    emailjs.sendForm(ghxn6gd,49pab4a, this)
-        .then(function() {
-            alert('Az üzenet sikeresen elküldve!');
-            document.getElementById('contact-form').reset();
-        }, function(error) {
-            alert('Hiba történt: ' + JSON.stringify(error));
-        });
-});
