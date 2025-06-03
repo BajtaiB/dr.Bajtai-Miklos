9383f0c8fc55a262e356638cf90962a42649b02f
Ügyvéd <bhercegno1@gmail.com>
2025-06-03 19:47:29 +0200

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 // Hamburger menü megjelenítése/elrejtése
 function toggleMenu() {
   const menu = document.getElementById("mobileMenu");
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
+  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
 }
 
 // Scroll animáció ScrollReveal-vel
-ScrollReveal().reveal('header', { delay: 200, origin: 'top', distance: '50px', duration: 800 });
-ScrollReveal().reveal('#hero', { delay: 300, origin: 'bottom', distance: '50px', duration: 800 });
-ScrollReveal().reveal('#rolam', { delay: 400, origin: 'left', distance: '50px', duration: 800 });
-ScrollReveal().reveal('#szolgaltatasok', { delay: 500, origin: 'right', distance: '50px', duration: 800 });
-ScrollReveal().reveal('#kapcsolat', { delay: 600, origin: 'bottom', distance: '50px', duration: 800 });
+ScrollReveal().reveal('header', {
+  delay: 200,
+  origin: 'top',
+  distance: '50px',
+  duration: 800
+});
+
+ScrollReveal().reveal('#hero', {
+  delay: 300,
+  origin: 'bottom',
+  distance: '50px',
+  duration: 800
+});
+
+ScrollReveal().reveal('#rolam', {
+  delay: 400,
+  origin: 'left',
+  distance: '50px',
+  duration: 800
+});
+
+ScrollReveal().reveal('#szolgaltatasok', {
+  delay: 500,
+  origin: 'right',
+  distance: '50px',
+  duration: 800
+});
+
+ScrollReveal().reveal('#kapcsolat', {
+  delay: 600,
+  origin: 'bottom',
+  distance: '50px',
+  duration: 800
+});
